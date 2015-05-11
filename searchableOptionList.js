848de789034ae79f69506298c5e46ff367098202
Patrick Bauerochse <bauerochse@pondus.de>
2015-05-11 17:32:42 +0200

    ok, position fixed was a stupid idea

--- searchableOptionList.js
+++ searchableOptionList.js
@@ -147,7 +147,6 @@
             // maximum of original <select> width and default <input> width
             var selectWidth = Math.max(this.input.outerWidth(false), $(this.element).outerWidth(false));
             this.input.css('width', selectWidth);
-            this.selectionContainer.css('width', (selectWidth + 50) * 1.2);    // 120%
 
             if ($.isFunction(this.settings.onRendered)) {
                 this.settings.onRendered.call(this);
