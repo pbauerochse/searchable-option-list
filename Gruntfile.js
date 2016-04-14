bdc931f32d449f375d2832c648a4aa495b303888
Patrick Bauerochse <patrick.bauerochse@wesustain.com>
2016-04-14 14:53:03 +0200

    distributing to separate files for development

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,10 +8,43 @@ module.exports = function (grunt) {
             all: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js']
         },
 
+        concat: {
+            dist: {
+                src: ['src/sol-data-option.js', 'src/sol-data-optiongroup.js', 'src/sol-core.js'],
+                dest: 'build/sol-<%= pkg.version %>.js',
+                options: {
+                    banner: ";(function ($, window, document, undefined) {\n 'use strict';\n",
+                    footer: "}(jQuery, window, document));"
+                }
+            }
+        },
+
         qunit: {
             all: ['test/**/*.html'],
             options: {
                 timeout: 10000
+            }
+        },
+
+        copy: {
+            dist: {
+                files: [
+                    {src: 'build/sol-<%= pkg.version %>.js', dest: 'dist/sol.js'}
+                ],
+            },
+        },
+
+        uglify: {
+            options: {
+                banner: '/* <%= pkg.name %> v<%= pkg.version %> (<%= grunt.template.today("yyyy-mm-dd") %>)\n' +
+                '/* <%= pkg.homepage %>\n' +
+                '/* Licensed under the <%= pkg.license %> license',
+                sourceMap: true,
+                sourceMapName: 'dist/sol.min.js.map'
+            },
+            build: {
+                src: 'dist/sol.js',
+                dest: 'dist/sol.min.js'
             }
         },
 
@@ -23,28 +56,8 @@ module.exports = function (grunt) {
             }
         },
 
-        copy: {
-            main: {
-                files: [
-                    {src: 'src/sol.src.js', dest: 'dist/sol.js'}
-                ],
-            },
-        },
-
-        uglify: {
-            options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
-                sourceMap: true,
-                sourceMapName: 'dist/sol.min.js.map'
-            },
-            build: {
-                src: 'src/sol.src.js',
-                dest: 'dist/sol.min.js'
-            }
-        },
-
         cssmin: {
-            minify: {
+            dist: {
                 src: 'dist/sol.css',
                 dest: 'dist/sol.min.css',
                 options: {
@@ -60,8 +73,9 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-concat');
 
     // Default task(s).
-    grunt.registerTask('default', ['jshint', 'qunit', 'sass', 'copy', 'uglify', 'cssmin']);
+    grunt.registerTask('default', ['jshint', 'concat', 'qunit', 'copy', 'uglify', 'sass', 'cssmin']);
 
 };
